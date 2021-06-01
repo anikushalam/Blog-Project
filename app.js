7dc14c12177392075413dbff8e3e8ae36057d438
anikushalam <rjsingh.ak@gmail.com>
2021-06-01 09:18:47 +0530

    Update secure false

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV !== "production") {
       saveUninitialized: true,
       cookie: {
         httpOnly: true,
-        secure: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 + 60 * 60 * 24 * 7,
       },
